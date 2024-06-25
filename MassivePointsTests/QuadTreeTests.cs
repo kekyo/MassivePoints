8bde1c38628668849d1e935a769fcf5500e1e2a4
Kouji Matsui <k@kekyo.net>
2024-06-25 11:10:02 +0900

    Added session on all tests.

--- MassivePointsTests/QuadTreeTests.cs
+++ MassivePointsTests/QuadTreeTests.cs
@@ -70,25 +70,34 @@ public sealed class QuadTreeTests
     {
         var quadTree = QuadTree.Factory.Create<long>(100000, 100000, maxNodePoints);
 
-        var points = new Point[count];
-        
-        var r = new Random();
-        for (var index = 0L; index < count; index++)
-        {
-            var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
-            points[index] = point;
-            await quadTree.AddAsync(point, index);
-        }
-        
-        for (var index = 0L; index < count; index++)
-        {
-            var point = points[index];
-            var results = await quadTree.LookupPointAsync(point);
+        await using var session = await quadTree.BeginSessionAsync(true);
 
-            var f1 = results.Any(entry => entry.Value == index);
-            Assert.That(f1, Is.True);
-            var f2 = results.All(entry => entry.Key.Equals(point));
-            Assert.That(f2, Is.True);
+        try
+        {
+            var points = new Point[count];
+
+            var r = new Random();
+            for (var index = 0L; index < count; index++)
+            {
+                var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
+                points[index] = point;
+                await quadTree.AddAsync(point, index);
+            }
+
+            for (var index = 0L; index < count; index++)
+            {
+                var point = points[index];
+                var results = await quadTree.LookupPointAsync(point);
+
+                var f1 = results.Any(entry => entry.Value == index);
+                Assert.That(f1, Is.True);
+                var f2 = results.All(entry => entry.Key.Equals(point));
+                Assert.That(f2, Is.True);
+            }
+        }
+        finally
+        {
+            await session.FinishAsync();
         }
     }
     
@@ -97,36 +106,45 @@ public sealed class QuadTreeTests
     {
         var quadTree = QuadTree.Factory.Create<long>(100000, 100000, maxNodePoints);
 
-        var points = new Point[count];
-        
-        var r = new Random();
-        for (var index = 0L; index < count; index++)
+        await using var session = await quadTree.BeginSessionAsync(true);
+
+        try
         {
-            var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
-            points[index] = point;
-            await quadTree.AddAsync(point, index);
+            var points = new Point[count];
+
+            var r = new Random();
+            for (var index = 0L; index < count; index++)
+            {
+                var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
+                points[index] = point;
+                await quadTree.AddAsync(point, index);
+            }
+
+            // Try random bounds lookup, repeats 100 times.
+            for (var index = 0; index < 100; index++)
+            {
+                var point = new Point(r.Next(0, 49999), r.Next(0, 49999));
+                var bound = new Bound(point, r.Next(0, 49999), r.Next(0, 49999));
+
+                var expected = points.
+                    Select((p, index) => (p, index)).
+                    Where(entry => bound.IsWithin(entry.p)).
+                    OrderBy(entry => entry.index).
+                    Select(entry => (long)entry.index).
+                    ToArray();
+
+                var results = await quadTree.LookupBoundAsync(bound);
+                var actual = results.
+                    Select(r => r.Value).
+                    OrderBy(index => index).
+                    ToArray();
+
+                Assert.That(actual, Is.EqualTo(expected));
+            }
         }
-        
-        // Try random bounds lookup, repeats 100 times.
-        for (var index = 0; index < 100; index++)
+        finally
         {
-            var point = new Point(r.Next(0, 49999), r.Next(0, 49999));
-            var bound = new Bound(point, r.Next(0, 49999), r.Next(0, 49999));
-
-            var expected = points.
-                Select((p, index) => (p, index)).
-                Where(entry => bound.IsWithin(entry.p)).
-                OrderBy(entry => entry.index).
-                Select(entry => (long)entry.index).
-                ToArray();
-            
-            var results = await quadTree.LookupBoundAsync(bound);
-            var actual = results.
-                Select(r => r.Value).
-                OrderBy(index => index).
-                ToArray();
-
-            Assert.That(actual, Is.EqualTo(expected));
+            await session.FinishAsync();
         }
     }
 
@@ -135,41 +153,50 @@ public sealed class QuadTreeTests
     {
         var quadTree = QuadTree.Factory.Create<long>(100000, 100000, maxNodePoints);
 
-        var points = new Point[count];
-        
-        var r = new Random();
-        for (var index = 0L; index < count; index++)
-        {
-            var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
-            points[index] = point;
-            await quadTree.AddAsync(point, index);
-        }
-        
-        // Try random bounds lookup, repeats 100 times.
-        for (var index = 0; index < 100; index++)
-        {
-            var point = new Point(r.Next(0, 49999), r.Next(0, 49999));
-            var bound = new Bound(point, r.Next(0, 49999), r.Next(0, 49999));
+        await using var session = await quadTree.BeginSessionAsync(true);
 
-            var expected = points.
-                Select((p, index) => (p, index)).
-                Where(entry => bound.IsWithin(entry.p)).
-                OrderBy(entry => entry.index).
-                Select(entry => (long)entry.index).
-                ToArray();
+        try
+        {
+            var points = new Point[count];
 
-            var results = new List<KeyValuePair<Point, long>>();
-            await foreach (var entry in quadTree.EnumerateBoundAsync(bound, default))
+            var r = new Random();
+            for (var index = 0L; index < count; index++)
             {
-                results.Add(entry);
+                var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
+                points[index] = point;
+                await quadTree.AddAsync(point, index);
             }
-            
-            var actual = results.
-                Select(r => r.Value).
-                OrderBy(index => index).
-                ToArray();
-            
-            Assert.That(actual, Is.EqualTo(expected));
+
+            // Try random bounds lookup, repeats 100 times.
+            for (var index = 0; index < 100; index++)
+            {
+                var point = new Point(r.Next(0, 49999), r.Next(0, 49999));
+                var bound = new Bound(point, r.Next(0, 49999), r.Next(0, 49999));
+
+                var expected = points.
+                    Select((p, index) => (p, index)).
+                    Where(entry => bound.IsWithin(entry.p)).
+                    OrderBy(entry => entry.index).
+                    Select(entry => (long)entry.index).
+                    ToArray();
+
+                var results = new List<KeyValuePair<Point, long>>();
+                await foreach (var entry in quadTree.EnumerateBoundAsync(bound, default))
+                {
+                    results.Add(entry);
+                }
+
+                var actual = results.
+                    Select(r => r.Value).
+                    OrderBy(index => index).
+                    ToArray();
+
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+        finally
+        {
+            await session.FinishAsync();
         }
     }
 
@@ -179,30 +206,39 @@ public sealed class QuadTreeTests
     {
         var quadTree = QuadTree.Factory.Create<long>(100000, 100000, maxNodePoints);
 
-        var points = new Dictionary<Point, List<long>>();
-        
-        var r = new Random();
-        for (var index = 0L; index < count; index++)
+        await using var session = await quadTree.BeginSessionAsync(true);
+
+        try
         {
-            var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
-            if (!points.TryGetValue(point, out var list))
+            var points = new Dictionary<Point, List<long>>();
+
+            var r = new Random();
+            for (var index = 0L; index < count; index++)
             {
-                list = new();
-                points.Add(point, list);
+                var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
+                if (!points.TryGetValue(point, out var list))
+                {
+                    list = new();
+                    points.Add(point, list);
+                }
+
+                list.Add(index);
+                await quadTree.AddAsync(point, index);
             }
 
-            list.Add(index);
-            await quadTree.AddAsync(point, index);
-        }
+            foreach (var entry in points)
+            {
+                var removed = await quadTree.RemovePointsAsync(entry.Key, performShrinking);
+                Assert.That(removed, Is.EqualTo(entry.Value.Count));
+            }
 
-        foreach (var entry in points)
+            var willEmpty = await quadTree.LookupBoundAsync(quadTree.Entire);
+            Assert.That(willEmpty.Length, Is.EqualTo(0));
+        }
+        finally
         {
-            var removed = await quadTree.RemovePointsAsync(entry.Key, performShrinking);
-            Assert.That(removed, Is.EqualTo(entry.Value.Count));
+            await session.FinishAsync();
         }
-
-        var willEmpty = await quadTree.LookupBoundAsync(quadTree.Entire);
-        Assert.That(willEmpty.Length, Is.EqualTo(0));
     }
 
     [TestCase(1000000, 1024, true)]
@@ -211,31 +247,40 @@ public sealed class QuadTreeTests
     {
         var quadTree = QuadTree.Factory.Create<long>(100000, 100000, maxNodePoints);
 
-        var points = new Dictionary<Point, List<long>>();
-        
-        var r = new Random();
-        for (var index = 0L; index < count; index++)
+        await using var session = await quadTree.BeginSessionAsync(true);
+
+        try
         {
-            var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
-            if (!points.TryGetValue(point, out var list))
+            var points = new Dictionary<Point, List<long>>();
+
+            var r = new Random();
+            for (var index = 0L; index < count; index++)
             {
-                list = new();
-                points.Add(point, list);
+                var point = new Point(r.Next(0, 99999), r.Next(0, 99999));
+                if (!points.TryGetValue(point, out var list))
+                {
+                    list = new();
+                    points.Add(point, list);
+                }
+
+                list.Add(index);
+                await quadTree.AddAsync(point, index);
             }
 
-            list.Add(index);
-            await quadTree.AddAsync(point, index);
-        }
+            var removed = 0L;
+            foreach (var childBound in quadTree.Entire.ChildBounds)
+            {
+                removed += await quadTree.RemoveBoundAsync(childBound, performShrinking);
+            }
+            Assert.That(removed, Is.EqualTo(count));
 
-        var removed = 0L;
-        foreach (var childBound in quadTree.Entire.ChildBounds)
+            var willEmpty = await quadTree.LookupBoundAsync(quadTree.Entire);
+            Assert.That(willEmpty.Length, Is.EqualTo(0));
+        }
+        finally
         {
-            removed += await quadTree.RemoveBoundAsync(childBound, performShrinking);
+            await session.FinishAsync();
         }
-        Assert.That(removed, Is.EqualTo(count));
-
-        var willEmpty = await quadTree.LookupBoundAsync(quadTree.Entire);
-        Assert.That(willEmpty.Length, Is.EqualTo(0));
     }
 
     //////////////////////////////////////////////////////////////////////////////////
